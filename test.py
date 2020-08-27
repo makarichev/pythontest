2711b9b7810678715e11f68cc85149b1d781c1d7
dmitrijmakarichev <dmitrij.makarichev@gmail.com>
2020-08-27 16:32:05 +0300

    git

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ print("hello world")
 print("hello world")
 print("hello world222")
 print("hello world222")
-print("hello world222")
-print("hello world222")
-print("hello world222")
-gbwiojmh[wpnhbopwhwp[orh]]
+
+
+
+
